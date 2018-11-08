75618b29b200bcd030c35a38c493b4cf44146aff
Wang <eeewl1250@163.com>
2018-11-08 22:01:33 +0800

    Initialize the words database

--- models/word.js
+++ models/word.js
@@ -1,16 +1,25 @@
-// const mongoose = require('mongoose')
-//
-// const wordSchema = new mongoose.Schema({
-//     word: String,
-//     mean: [{
-//         class: String,
-//         gender: { type: String, default: '' },
-//         plural: { type: String, default: '' },
-//         mean: String,
-//         example: [Schema.Types.objectId]
-//     }]
-// })
-//
-// wordSchema.index({ word: 1 })
-//
-// module.exports = mongoose.model('word', wordSchema)
+const mongoose = require('mongoose')
+
+const wordSchema = new mongoose.Schema({
+    word: String,
+    mean: [{
+        chinese: { type: String, default: '' },
+        english: { type: String, default: '' },
+        class: { type: String, default: '' },
+        nominative: { type: String, default: '' },
+        genitive: {type: String, default: ''},
+        plural: { type: String, default: '' },
+        pic: [{
+            ref: 'pic',
+            type: mongoose.Schema.Types.ObjectId
+        }],
+        examples: [{
+            ref: 'example',
+            type: mongoose.Schema.Types.ObjectId
+        }]
+    }]
+})
+
+wordSchema.index({ word: 1 })
+
+module.exports = mongoose.model('word', wordSchema)
--- router/word.js
+++ router/word.js
@@ -1,9 +1,63 @@
 const express = require('express')
+const router = express.Router()
+
+const path = require('path')
+
 const Example = require('../models/example')
 const Book = require('../models/book')
 const Word = require('../models/word')
 
-const router = express.Router()
+// 文件操作
+const lineByLine = require('n-readlines')
+const liner = new lineByLine(path.resolve(__dirname, '../assets/dict/geWord.txt'))
+
+// 初始化单词库，导入单词及名词一二格与复数
+router.post('/init', (req, res) => {
+    let line
+    let lineNumber = 0
+
+    let words = []
+
+    while (line = liner.next()) {
+        console.log('Line ' + lineNumber)
+
+        let arr = line.toString().split('|')
+        let word = arr[0]
+        let nominative = (arr[1] ? arr[1] : '').match(/(die|der|das)/) ? arr[1] : ''
+        let wClass = nominative ? 'noun' : ''
+
+        let pArr = (arr[2] ? arr[2] : '').split(/,\s*/)
+        let genitive = pArr[0]
+        let plural = pArr[1] ? pArr[1] : pArr[0]
+
+        words.push({
+            word: word,
+            mean: [{
+                class: wClass,
+                nominative: nominative,
+                genitive: genitive,
+                plural: plural
+            }]
+        })
+
+        lineNumber++
+    }
+
+    Word.insertMany(words, (err) => {
+        if (err) {
+            console.log('Words Init Error: ' + err)
+            res.json({
+                code: 0,
+                msg: 'Post-end Error.'
+            })
+        } else {
+            res.json({
+                code: 1,
+                msg: 'Words Insert Success.'
+            })
+        }
+    })
+})
 
 router.post('/word', (req, res) => {
     Word.create({
